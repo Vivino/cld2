91c4ec50bdd70b684070ac2eb1fa64f8754aa5d8
Klaus Post <klauspost@gmail.com>
2017-05-13 20:36:01 +0200

    Add custom plugin loader function.

--- cld2.go
+++ cld2.go
@@ -6,7 +6,11 @@
 // For more information about CLD2, see https://github.com/CLD2Owners/cld2
 package cld2
 
-import "github.com/klauspost/cld2/internal/info"
+import (
+	"errors"
+
+	"github.com/klauspost/cld2/internal/info"
+)
 
 // Detect returns the language code for detected language
 // in the given text.
@@ -21,6 +25,20 @@ var DetectLang = func(text string) Language { return UNKNOWN_LANGUAGE }
 // Extended languages are enabled.
 // Unknown languages are removed from the resultset.
 var DetectThree = func(text string) Languages { return Languages{} }
+
+// ErrNoPlugins is returned if attempting to load plugin on non-supported platform.
+var ErrNoPlugins = errors.New("CLD2: Architecture does not support plugins")
+
+// LoadPlugin can be used to load plugin on Linux and Go 1.8.
+// If support has been compiled into the binary calling this will have no effect.
+// If binary was compiled without CGO and platform does not support plugins ErrNoPlugins
+// will be returned.
+var LoadPlugin = func(path string) error {
+	if Enabled {
+		return nil
+	}
+	return ErrNoPlugins
+}
 
 // Enabled will be set to true if compiled with cgo, or plugin loaded
 // successfully
--- cld2_linux.go
+++ cld2_linux.go
@@ -16,36 +16,60 @@ import (
 //go:generate go build -buildmode=plugin -o lib/cld2go.so github.com/klauspost/cld2/internal/plugin
 
 func init() {
+	LoadPlugin = func(path string) error {
+		if Enabled {
+			return nil
+		}
+		p, err := plugin.Open(paths[i])
+		if err != nil {
+			return err
+		}
+		return usePlugin(p)
+
+	}
 	p, err := findPlugin("/lib", "lib", ".", filepath.Join(os.Getenv("GOPATH"), "src", "github.com", "klauspost", "cld2", "lib"))
 	if err != nil {
-		log.Printf("CLD2: Error loading plugin: %v", err)
+		log.Printf("CLD2: Loading plugin: %v", err)
 		return
 	}
+	err := usePlugin(p)
+	if err != nil {
+		log.Printf("CLD2: plugin init: %v", err)
+		return
+	}
+}
 
+func usePlugin(p *plugin.Plugin) error {
 	f, err := p.Lookup("PluginDetect")
 	if err != nil {
-		log.Printf("CLD2: Load Detect: %v", err)
-		return
+		return err
 	}
-	Detect = f.(func(text string) string)
+	var ok bool
+	Detect, ok = f.(func(text string) string)
+	if !ok {
+		return fmt.Errorf("PluginDetect: wrong signature: %T", f)
+	}
 
 	f, err = p.Lookup("PluginDetectLang")
 	if err != nil {
-		log.Printf("CLD2: Load DetectLang: %v", err)
-		return
+		return err
 	}
-	dl := f.(func(text string) uint16)
+	dl, ok := f.(func(text string) uint16)
+	if !ok {
+		return fmt.Errorf("PluginDetectLang: wrong signature: %T", f)
+	}
 	DetectLang = func(text string) Language {
 		return Language(dl(text))
 	}
 
 	f, err = p.Lookup("PluginDetectThree")
 	if err != nil {
-		log.Printf("CLD2: Load DetectThree: %v", err)
-		return
+		return err
 	}
-
-	dt := f.(func(text string) info.Languages)
+	dt, ok := f.(func(text string) info.Languages)
+	if !ok {
+		return fmt.Errorf("PluginDetectThree: wrong signature: %T", f)
+	}
 	DetectThree = func(text string) Languages {
 		return infoToLanguages(dt(text))
 	}
